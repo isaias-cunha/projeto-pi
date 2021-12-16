e43ecf2532c1dafeb4092bb9c6fd2968949430a1
isaias-cunha <isaiascunha13@gmail.com>
2021-12-16 14:04:26 -0300

    criação da class cliente

---
+++ src/main/java/projeto/pi/models/cliente.java
@@ -0,0 +1,48 @@
+package projeto.pi.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+
+public class cliente {
+	
+	private Long id;
+	private String email;
+	private String senha;
+	private String nome;
+	private String endereco;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	
+	
+}
