e4228e9a27edd80bc212dbe94448c13b805f1d05
PedroHenrique547 <weslleya257@gmail.com>
2021-12-16 16:47:04 -0300

    luiza

--- src/main/java/projeto/pi/loja/controllers/LojaController.java
+++ src/main/java/projeto/pi/loja/controllers/LojaController.java
@@ -3,7 +3,7 @@ package projeto.pi.loja.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@Controller.
 public class LojaController {
 
 	@RequestMapping("/loja/cliente")
--- src/main/java/projeto/pi/models/cliente.java
+++ src/main/java/projeto/pi/models/cliente.java
@@ -6,7 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class cliente {
+public class cliente {errer
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
